31dac89c89a753a4d58897dae20b579872b70870
Gonzalo Etse <gonzaloetjo@gmail.com>
2020-01-25 12:04:03 +0100

    search-seed2

--- app/models/pleureuse.rb
+++ app/models/pleureuse.rb
@@ -19,7 +19,6 @@ class Pleureuse < ApplicationRecord
   validates :city, presence: true
   validates :gender, presence: true
   validates :price, presence: true
-  validates :picture, presence: true
 
   GENDER = ['Male', 'Female']
   DRAMA = ['Maitresse', 'Accusation', 'Se roule par terre']
