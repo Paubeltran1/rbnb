ae54cbefaf79b324dba3b45ddfc10c14fe295eb9
Gonzalo Etse <gonzaloetjo@gmail.com>
2020-01-25 14:49:09 +0100

    cambios

--- app/controllers/pleureuses_controller.rb
+++ app/controllers/pleureuses_controller.rb
@@ -3,11 +3,12 @@ skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
      @pleureuse = policy_scope(Pleureuse).all
-    if params[:search].present?
-      @pleureuses = Pleureuse.perform_search(params[:search])
+    if params[:search].present? || params[:drama_option].present?
+      @pleureuses = Pleureuse.perform_search("#{params[:search]} #{params[:drama_option]}")
     else
       @pleureuses = Pleureuse.all
     end
+
     @photos = ["https://images.unsplash.com/photo-1573516515928-92444ec46ce2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1555&q=80","https://images.unsplash.com/photo-1566677379359-5ef1321fcb7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80","https://images.unsplash.com/photo-1551823934-61391394a6ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1993&q=80","https://images.unsplash.com/photo-1545792583-533185490f52?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=958&q=80","https://images.unsplash.com/photo-1567659893051-0b046978d0f0?ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80", "https://images.unsplash.com/photo-1533392151650-269f96231f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80","https://images.unsplash.com/photo-1546189827-c4cd7d1cc07d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80","https://images.unsplash.com/photo-1551823934-3d82b4d2fd1d?ixlib=rb-1.2.1&auto=format&fit=crop&w=2010&q=80"]
   end
 
